e068616458da6b4251402357844d740deea239d2
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-05-28 00:08:31 +0800

    chore(frontend): update folder structure and add import alias

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store.jsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.jsx';
+import router from './routes/RouteBase';
+import './index.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
