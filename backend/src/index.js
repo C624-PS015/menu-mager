f4e7093dcab3d923633f1aa98be3a1542e4804b5
yoviarpauzi <yoviarpauzi99@gmail.com>
2024-05-30 00:10:21 +0700

    feat(router): add default port

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 import app from "./apps/web.js";
 import logger from "./apps/logger.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  logger.info("Server running with port 9000");
+    logger.info("Server running with port 9000");
 });
